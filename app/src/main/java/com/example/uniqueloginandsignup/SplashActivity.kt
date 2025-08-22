bb8011ca5d21ad7f6b16c2b7a5b886a994765c2f
Ujjaval Saini <sainiujvl@gmail.com>
2025-08-22 10:08:48 +0530

    login screen completed

--- app/src/main/java/com/example/uniqueloginandsignup/SplashActivity.kt
+++ app/src/main/java/com/example/uniqueloginandsignup/SplashActivity.kt
@@ -1,6 +1,9 @@
 package com.example.uniqueloginandsignup
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,10 +14,13 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+
+        },3000)
+//
     }
 }
