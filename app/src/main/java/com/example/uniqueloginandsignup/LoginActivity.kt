e96f580d2da061ad5809352dd9cdb0caca5f915c
Ujjaval Saini <sainiujvl@gmail.com>
2025-08-23 00:38:26 +0530

    backend added for login and signup

--- app/src/main/java/com/example/uniqueloginandsignup/LoginActivity.kt
+++ app/src/main/java/com/example/uniqueloginandsignup/LoginActivity.kt
@@ -1,12 +1,17 @@
 package com.example.uniqueloginandsignup
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.InputType
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 import com.google.android.material.textfield.TextInputEditText
 
 class LoginActivity : AppCompatActivity() {
@@ -20,6 +25,24 @@ class LoginActivity : AppCompatActivity() {
 
         val etEmailOrMobile = findViewById<TextInputEditText>(R.id.etEmailOrMobile)
         val tvLoginWithMobile = findViewById<TextView>(R.id.tvLoginWithMobile)
+
+        val signUpPage = findViewById<MaterialButton>(R.id.signUpPage)
+        signUpPage.setOnClickListener {
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setMessage("Please wait a sec...")
+            progressDialog.setCancelable(false) // back press se cancel na ho
+            progressDialog.show()
+
+            Handler(Looper.getMainLooper()).postDelayed({
+                progressDialog.dismiss()
+
+                val intent = Intent(this, SignUpActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 1000) // 2 second delay
+
+        }
+
 
         tvLoginWithMobile.setOnClickListener {
             if (!isMobileLogin) {
--- app/src/main/java/com/example/uniqueloginandsignup/SignUpActivity.kt
+++ app/src/main/java/com/example/uniqueloginandsignup/SignUpActivity.kt
@@ -1,20 +1,38 @@
 package com.example.uniqueloginandsignup
 
+import android.app.ProgressDialog
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.button.MaterialButton
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_sign_up)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val signInPage = findViewById<MaterialButton>(R.id.signInPage)
+
+        signInPage.setOnClickListener {
+            val progressDialog = ProgressDialog(this)
+            progressDialog.setMessage("Please wait a sec...")
+            progressDialog.setCancelable(false)
+            progressDialog.show()
+            Handler(Looper.getMainLooper()).postDelayed({
+                progressDialog.dismiss()
+
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent)
+                finish()
+            }, 1000)
+
         }
+
     }
 }
